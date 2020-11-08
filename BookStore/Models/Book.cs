9febc02523ab9920dc8e7f6ef8bc7805956b60d8
lukmaan-b <lukmaan.butt@gmail.com>
2020-11-08 22:35:47 +0000

    Add: BookController, Book Model and Index View

---
+++ BookStore/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string BookName { get; set; }
+        public int Stock { get; set; }
+        public string Author { get; set; }
+    }
+}
